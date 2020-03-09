8368865207645d9e0514d8c9dfa71d1e69898d93
Io Dodon <iondodon2@gmail.com>
2020-03-09 21:05:52 +0200

    curl

--- actor_model/lib/actor_model.ex
+++ actor_model/lib/actor_model.ex
@@ -1,8 +1,24 @@
 defmodule ActorModel do
 
   def main do
-    {:ok, pid} = EventsourceEx.new("https://localhost:4000", stream_to: self)
+    System.cmd(
+      "curl", ["--silent", "http://localhost:4000/iot"],
+      into:  Collectable.into(MapSet.new())
+    )
+  end
+
+end
+
+defimpl Collectable, for: MapSet do
+  def into(original) do
+    collector_fun = fn
+      set, {:cont, elem} -> MapSet.put(set, elem)
+      set, :done -> set
+      _set, :halt -> :ok
+    end
+
+    {original, collector_fun}
   end
 end
 
-IO.puts(ActorModel.main())
+IO.puts(ActorModel.main)
--- actor_model/mix.exs
+++ actor_model/mix.exs
@@ -11,19 +11,15 @@ defmodule ActorModel.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:eventsource_ex, "~> 0.0.2"}
+      {:eventsource_ex, git: "https://github.com/cwc/eventsource_ex.git"}
     ]
   end
 end
