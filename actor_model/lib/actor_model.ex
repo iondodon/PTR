ed53def145d714787868da48cc48d20922146f4e
Io Dodon <iondodon2@gmail.com>
2020-03-10 10:27:02 +0200

    IO.stream

--- actor_model/lib/actor_model.ex
+++ actor_model/lib/actor_model.ex
@@ -3,22 +3,11 @@ defmodule ActorModel do
   def main do
     System.cmd(
       "curl", ["--silent", "http://localhost:4000/iot"],
-      into:  Collectable.into(MapSet.new())
+      into:  IO.stream(:stdio, :line)
     )
   end
 
 end
 
-defimpl Collectable, for: MapSet do
-  def into(original) do
-    collector_fun = fn
-      set, {:cont, elem} -> MapSet.put(set, elem)
-      set, :done -> set
-      _set, :halt -> :ok
-    end
-
-    {original, collector_fun}
-  end
-end
 
 IO.puts(ActorModel.main)
