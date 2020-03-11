b5d9510315609d7554f4e9e314f294fb5133e017
Io Dodon <iondodon2@gmail.com>
2020-03-11 22:37:00 +0200

    update

--- actor_model/lib/actor_model.ex
+++ actor_model/lib/actor_model.ex
@@ -1,21 +1,23 @@
 defmodule ActorModel do
-  import IEx.Helpers
 
   def show(id, event, data, dispatch_ts) do
-    
+    IO.puts(id)
+    IO.puts(event)
+    IO.puts(data)
+    IO.puts(dispatch_ts)
   end
 
-  def handle do
+  def handle() do
     receive do
-      %EventsourceEx.Message{id: id, event: event, data: data, dispatch_ts: dispatch_ts} 
+      %EventsourceEx.Message{id: id, event: event, data: data, dispatch_ts: dispatch_ts}
         -> show(id, event, data, dispatch_ts)
     end
-    handle
+    handle()
   end
 
   def main do
     {:ok, pid} = EventsourceEx.new("http://localhost:4000/iot", stream_to: self())
-    handle
+    handle()
   end
 
 end
