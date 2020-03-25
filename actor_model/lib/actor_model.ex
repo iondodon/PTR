aa1cc3eecc2b3d5db4258e75c229f2671bf2facf
Io Dodon <iondodon2@gmail.com>
2020-03-25 22:05:34 +0200

    changes

--- actor_model/lib/actor_model.ex
+++ actor_model/lib/actor_model.ex
@@ -1,33 +1,30 @@
 defmodule ActorModel do
 
-  def prepare_supervisors() do
-    # set event-handlers supervisor
-    Supervisor.start_link([ {Task.Supervisor, name: EventHandler} ], strategy: :one_for_one)
-
-    # set ForecastStation and StateManager supervisor
-    children = [ { ForecastStation, %{} }, { StateManager, %{} } ]
-    Supervisor.start_link(children, strategy: :one_for_all)
-  end
-
-  def handle_event(new_state) do
-    {:ok, pid} = Task.Supervisor.start_child(EventHandler, fn pid ->
-      case elem(new_state[:data], 0) do
-        :ok -> StateManager.start_link(new_state)   # the ok case
-        :error -> Process.exit(pid, :kill)          # the panic case
-      end
+  def handle_event(supervisor_pid, new_state) do
+    event_handler = Task.Supervisor.async(supervisor_pid, fn ->
+      StateManager.start_link(new_state)
     end)
+
+    if elem(new_state[:data], 0) === :error do
+      Task.shutdown(event_handler, 0)
+    end
   end
 
   def wait_for_event() do
+    # set event-handlers supervisor
+    {:ok, supervisor_pid} = Task.Supervisor.start_link()
+
     receive do
       %EventsourceEx.Message{id: id, event: event, data: data, dispatch_ts: dispatch_ts}
-        -> handle_event(%{:id => id, :event => event, :data => JSON.decode(data), :dispatch_ts => dispatch_ts })
+        -> handle_event(supervisor_pid, %{:id => id, :event => event, :data => JSON.decode(data), :dispatch_ts => dispatch_ts })
     end
     wait_for_event()
   end
 
   def run() do
-    prepare_supervisors()
+    # set ForecastStation and StateManager supervisor
+    children = [ { ForecastStation, %{} }, { StateManager, %{} } ]
+    Supervisor.start_link(children, strategy: :one_for_all)
 
     {:ok, pid} = EventsourceEx.new("http://localhost:4000/iot", stream_to: self())
 
